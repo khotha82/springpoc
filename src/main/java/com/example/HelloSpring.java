b8fe0fd8df44b4213ac541d72926275c1f78637d
khotha82 <khotha81@gmail.com>
2016-07-27 15:46:03 -0400

    add

--- src/main/java/com/example/HelloSpring.java
+++ src/main/java/com/example/HelloSpring.java
@@ -4,4 +4,8 @@ package com.example;
  * Created by krishna_hotha on 7/27/16.
  */
 public class HelloSpring {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
